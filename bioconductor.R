01ea450d173372e963b90d2dc7f86b0e9162f4d1
Noel Namai <noelnamai@fas.harvard.edu>
2016-02-09 15:38:40 -0500

    Create bioconductor.R

---
+++ bioconductor.R
@@ -0,0 +1,11 @@
+##updates bioconductor
+source('https://bioconductor.org/biocLite.R')
+biocLite()
+biocLite('Biobase')
+biocLite('methyAnalysis', dependencies=TRUE)
+biocLite('lumi', dependencies=TRUE) 
+biocLite('skewr', dependencies=TRUE)
+biocLite('methylumi', dependencies=TRUE)
+biocLite('FDb.InfiniumMethylation.hg19', dependencies=TRUE)
+biocLite('IlluminaHumanMethylation450kanno.ilmn12.hg19', dependencies=TRUE)
+biocLite('TxDb.Hsapiens.UCSC.hg19.knownGene', dependencies=TRUE)
